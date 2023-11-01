382f8f077de02782d115473a2f5d142d545f24de
Daniel Sanchez <dst990502@outlook.com>
2023-11-01 17:21:32 -0600

    Initial Commit

---
+++ NotNetflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotNetflix
+//
+//  Created by Iván Sánchez Torres on 01/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
