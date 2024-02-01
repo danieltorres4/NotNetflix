81bd36e45b9947e0a9e1326042c426db81fa44d7
Daniel Sanchez <dst990502@outlook.com>
2024-02-01 13:32:43 -0600

    [UPDATE] passing data to the collection view

--- NotNetflix/Controllers/HomeViewController.swift
+++ NotNetflix/Controllers/HomeViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+enum Sections: Int {
+    case TrendingMovies = 0
+    case TrendingTV = 1
+    case Popular = 2
+    case UpcomingMovies = 3
+    case TopRated = 4
+}
+
 class HomeViewController: UIViewController {
     
     let sectionTitles: [String] = ["Trending Movies", "Trending TV", "Popular", "Upcoming Movies", "Top Rated"]
@@ -29,7 +37,6 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
         homeFeedTable.tableHeaderView = headerView
         
-        fetchData()
     }
     
     override func viewDidLayoutSubviews() {
@@ -49,30 +56,6 @@ class HomeViewController: UIViewController {
         ]
         navigationController?.navigationBar.tintColor = .white
     }
-    
-    private func fetchData() {
-        /*APICaller.shared.getTrendingMovies { results in
-            switch results {
-            case .success(let movies):
-                print("Movies: \n\(movies)")
-            case .failure(let error):
-                print("Error: \n\(error)")
-            }
-        }
-        APICaller.shared.getTrendingTvs { results in
-            
-        }
-        APICaller.shared.getUpcomingMovies { results in
-            
-        }
-        APICaller.shared.getPopularMovies { results in
-            
-        }*/
-        APICaller.shared.getTopRated { _ in
-            
-        }
-        
-    }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -86,6 +69,56 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        switch indexPath.section {
+        case Sections.TrendingMovies.rawValue:
+            APICaller.shared.getTrendingMovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        case Sections.TrendingTV.rawValue:
+            APICaller.shared.getTrendingTvs { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        case Sections.Popular.rawValue:
+            APICaller.shared.getPopularMovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        case Sections.UpcomingMovies.rawValue:
+            APICaller.shared.getUpcomingMovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        case Sections.TopRated.rawValue:
+            APICaller.shared.getTopRated { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        default:
             return UITableViewCell()
         }
         
