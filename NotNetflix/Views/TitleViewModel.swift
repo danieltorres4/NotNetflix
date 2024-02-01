2bae615ac4be06c58e85d949dff3537463afcc6b
Daniel Sanchez <dst990502@outlook.com>
2024-02-01 16:31:54 -0600

    [UPDATE] title view model was created

---
+++ NotNetflix/Views/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NotNetflix
+//
+//  Created by Iván Sánchez Torres on 01/02/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
