e779b48f3728d42e078bbea578fbc9e3874ba8d6
Daniel Sanchez <dst990502@outlook.com>
2024-02-01 17:12:07 -0600

    [UPDATE] getSearchMovies method was created

--- NotNetflix/Controllers/SearchViewController.swift
+++ NotNetflix/Controllers/SearchViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    private var titles: [Title] = [Title]()
     private let searchTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
@@ -27,26 +28,50 @@ class SearchViewController: UIViewController {
         view.addSubview(searchTableView)
         searchTableView.delegate = self
         searchTableView.dataSource = self
+        
+        fetchSearchMovies()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         searchTableView.frame = view.bounds
     }
+    
+    func fetchSearchMovies() {
+        APICaller.shared.getSearchMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.searchTableView.reloadData()
+                }
+            case .failure(let error):
+                print("Error while Searching: \(error.localizedDescription)")
+            }
+        }
+        
+    }
 
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
             return UITableViewCell()
         }
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown Name", posterURL: title.poster_path ?? "")
+        cell.configure(with: model)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
     
     
--- NotNetflix/Managers/APICaller.swift
+++ NotNetflix/Managers/APICaller.swift
@@ -107,4 +107,22 @@ class APICaller {
         }
         task.resume()
     }
+    
+    func getSearchMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/movie/top_rated?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&width_watch_monetization_types=flatrate") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                print("SEARCH MOVIE RESULT: \n\(results.results)")
+                completion(.success(results.results))
+            } catch {
+                print("ERROR: \n\(error.localizedDescription)")
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        task.resume()
+    }
 }
