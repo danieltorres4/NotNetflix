9c5e96d2419d154e05256e3912b3cfdd5e89f7ed
Daniel Sanchez <dst990502@outlook.com>
2024-02-01 16:52:53 -0600

    [UPDATE] table view was created

--- NotNetflix/Controllers/SearchViewController.swift
+++ NotNetflix/Controllers/SearchViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let searchTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(searchTableView)
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchTableView.frame = view.bounds
     }
 
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    
 }
