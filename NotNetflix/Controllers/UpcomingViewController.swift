287a1a8addbdc72cc809fdf3e7d7b57fe004821d
Daniel Sanchez <dst990502@outlook.com>
2024-02-01 13:57:44 -0600

    [UPDATE] upcoming table view was created

--- NotNetflix/Controllers/UpcomingViewController.swift
+++ NotNetflix/Controllers/UpcomingViewController.swift
@@ -8,9 +8,62 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    private var titles: [Title] = [Title]()
+    
+    private let upcomingTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { result in
+            switch result {
+            case .success(let titles):
+                self.titles = titles
+                DispatchQueue.main.async {
+                    self.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print("Error UpcomingTitles: \(error.localizedDescription)")
+            
+            }
+        }
+    }
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
+        
+        return cell
+    }
+    
+    
 }
